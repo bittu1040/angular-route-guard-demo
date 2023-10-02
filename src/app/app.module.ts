216e5f6e18b4a3b2684c7016d58b72edacbb029c
bittu1040 <32195733+bittu1040@users.noreply.github.com>
2023-10-03 02:19:08 +0530

    resolve guard implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { DataService } from './services/data.service';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
-  providers: [],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsComponent],
+  providers: [DataService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
